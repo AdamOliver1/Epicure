2cad1ee0b728f365d1543d09a6e18c431eeea58a
adammoveo <adamo@moveo.co.il>
2023-01-08 10:31:01 +0200

    maxWidth

--- src/components/sharedComponents/WidthContainer/WidthContainer.tsx
+++ src/components/sharedComponents/WidthContainer/WidthContainer.tsx
@@ -7,26 +7,7 @@ type WidthContainerProps = {
   maxWidth: IMaxWidth;
 };
 
-// export enum MaxWidths {
-//   Small = 320,
-//   Medium = 768,
-//   Large = 1024,
-//   xLarge = 1188,
-//   xxLarge = 1440,
-// }
-
 export const WidthContainer = ({ children, maxWidth }: WidthContainerProps) => {
-  // const getWidth = (maxWidth: MaxWidths) => {
-  //   return maxWidth === MaxWidths.Small
-  //     ? style.small
-  //     : maxWidth === MaxWidths.Medium
-  //     ? style.medium
-  //     : maxWidth === MaxWidths.Large
-  //     ? style.large
-  //     : maxWidth === MaxWidths.xLarge
-  //     ? style.xLarge
-  //     : style.xxLarge;
-  // };
 
   return (
     <div className={`${style.container} ${maxWidth.getWidth()}`}>
